0e9db54cf00e39e31418089e673c4f0b76124e6f
Poulad Ashrafpour <poulad.pld@gmail.com>
2018-11-25 07:08:12 +0000

    Custom types

---
+++ 3.2.enums.rs
@@ -0,0 +1,36 @@
+/*
+clear; rustc --crate-name enums 3.2.enums.rs && ./enums
+*/
+
+enum WebEvent {
+    PageLoaded,
+    KeyPressed(char),
+    Click { x: u32, y:u32 },
+    PageUnloaded,
+}
+
+fn handle_event(e: WebEvent) {
+    use WebEvent::*;
+
+    match e {
+        PageLoaded => {
+            println!("Page loaded.")
+        },
+        KeyPressed(c) => println!("Key {:?} was pressed.", c),
+        Click{ x, y } => {
+            println!("User clicked at the point ({}, {}).", x, y)
+        },
+        _ => println!("Some unexpected event happened!")
+    }
+}
+
+fn main() {
+    use WebEvent::{PageLoaded, KeyPressed};
+
+    handle_event(PageLoaded);
+    handle_event(KeyPressed('a'));
+    handle_event(WebEvent::Click{ x: 100, y: 200 });
+    handle_event(WebEvent::PageUnloaded);
+
+    println!("WebEvent::PageLoaded has an i32 value of {}.", PageLoaded as i32);
+}
--- README.md
+++ README.md
@@ -6,5 +6,25 @@ me practicing [Rust by example]
 
 1. [X] Hello World
 1. [X] Primitives
-1. [ ] Custom Types
+1. [X] Custom Types
 1. [ ] Variable Bindings
+1. [ ] Types
+1. [ ] Conversion
+1. [ ] Expressions
+1. [ ] Flow Control
+1. [ ] Functions
+1. [ ] Modules
+1. [ ] Crates
+1. [ ] Cargo
+1. [ ] Attributes
+1. [ ] Generics
+1. [ ] Scoping Rules
+1. [ ] Traits
+1. [ ] macro_rules!
+1. [ ] Error handling
+1. [ ] std lib types
+1. [ ] std misc
+1. [ ] Testing
+1. [ ] Unsafe
+1. [ ] Compatiblity
+1. [ ] Meta
